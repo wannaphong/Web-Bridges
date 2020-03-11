cb4264202b6673409cf6be3a46aafbcff6228769
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-11 09:11:56 +0700

    Add file

---
+++ WebBridges/app/src/main/java/th/ac/kku/cis/mobileapp/webbridges/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package th.ac.kku.cis.mobileapp.webbridges.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
